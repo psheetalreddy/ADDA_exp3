ec30c97b236cf0fab31f8b2f0bef652f9392a350
P Sheetal Reddy <162999370+psheetalreddy@users.noreply.github.com>
2025-04-14 21:07:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from Kubernetes!');
+  res.end('Hello, This is Sheetal!');
 });
 
 server.listen(8080);
